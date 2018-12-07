b9e3d733c95930589a8dc3070aff91aefdbe62eb
walletcrea <44665914+walletcrea@users.noreply.github.com>
2018-12-08 00:00:56 +0300

    Update escrow.js

--- escrow.js
+++ escrow.js
@@ -29,20 +29,7 @@ var con = mysql.createConnection({
     database:"bz5lysrpv"
 });
 
-//server
-// Load the http module to create an http server.
-const http = require('http');
-
-// Configure our HTTP server to respond with Hello World to all requests.
-const server = http.createServer((request, response) => {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
-
-// Last, but not least, listen on port 8080
-// The environment variable PORT is automatically defined and equals to 8080
-server.listen(process.env.PORT, '0.0.0.0');
-
+//s
 
 
 
@@ -52,7 +39,7 @@ server.listen(process.env.PORT, '0.0.0.0');
 
 const {createServer} = require('http')
 const server = createServer(() => {})
-server.listen(8080)
+server.listen(0.0.0.0')
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
